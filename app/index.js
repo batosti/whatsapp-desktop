561314c542f68122eafda07aaf1588079ec73265
Andre Batosti <andre.batosti@raizen.com>
2024-05-20 16:56:02 -0300

    Add deeplink support

--- app/index.js
+++ app/index.js
@@ -6,6 +6,11 @@ const contextMenu = require('electron-context-menu');
 const appUrl = 'https://web.whatsapp.com'
 
 /**
+ * @type {BrowserWindow}
+ */
+let window = null;
+
+/**
  * @param {Electron.HandlerDetails} details 
  * @returns {action: 'deny'}
  */
@@ -15,20 +20,60 @@ function onNewWindow(details) {
 }
 
 const createWindow = () => {
-  const window = new BrowserWindow({
+  window = new BrowserWindow({
     icon: path.join(__dirname, 'assets/icons/64x64.png')
   })
   window.loadURL(appUrl, { userAgent: config.userAgent })
 
 	window.webContents.setWindowOpenHandler(onNewWindow);
-
 }
 
 contextMenu({
 	showSaveImageAs: true
 });
 
-app.whenReady().then(() => {
-  app.setAsDefaultProtocolClient('whatsapp')
-  createWindow()
-})
+const appLock = app.requestSingleInstanceLock();
+
+const protocolClient = 'whatsapp';
+if (!app.isDefaultProtocolClient(protocolClient, process.execPath)) {
+	app.setAsDefaultProtocolClient(protocolClient, process.execPath);
+}
+
+if (!appLock) {
+	app.quit();
+} else {
+  app.on('second-instance', onAppSecondInstance);
+  app.on('ready', onAppReady);
+}
+async function onAppReady() {
+	createWindow();
+};
+
+function processArgs(args) {
+	var regHttps = /^https:\/\/web\.whatsapp\.com\/.*/g;
+	var regWapp = /^whatsapp:\/.*/g;
+	for (const arg of args) {
+		if (regHttps.test(arg)) {
+			return arg;
+		}
+		if (regWapp.test(arg)) {
+			return appUrl + arg.substring(10, arg.length);
+		}
+	}
+}
+
+function onAppSecondInstance(event, args) {
+	console.debug('second-instance started');
+	if (window) {
+		event.preventDefault();
+		const url = processArgs(args);
+		if (url) {
+			allowFurtherRequests = false;
+			setTimeout(() => { allowFurtherRequests = true; }, 5000);
+			window.loadURL(url, { userAgent: config.chromeUserAgent });
+		}
+
+    window.focus();
+  }
+};
+
